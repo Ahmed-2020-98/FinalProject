47d23427d51624ac410533b2242d75f81b838869
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-07-31 18:59:13 +0200

    m

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Footer.css'
+import { FaApple,FaGooglePlay } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsArrowRightSquareFill } from "react-icons/bs";
 export default function Footer() {
   return (
     <>
@@ -14,28 +17,28 @@ export default function Footer() {
                         <p>Raya Installments</p>
                     </div>
                     <div>
-                        <h4>About Raya</h4>
-                        <p>Who we are</p>
-                        <p>Store Locator</p>
-                        <p>Raya Installments</p>
+                        <h4>Customer Support</h4>
+                        <p>Live Chat</p>
+                        <p>FAQs</p>
+                        <p>Contact Us</p>
                     </div>
                     <div>
-                        <h4>About Raya</h4>
-                        <p>Who we are</p>
-                        <p>Store Locator</p>
-                        <p>Raya Installments</p>
+                        <h4>More</h4>
+                        <p>Returns and Refund</p>
+                        <p>Terms and Conditions</p>
+                        <p>Privacy Policy</p>
                     </div>
                     <div>
-                        <h4>About Raya</h4>
-                        <p>Who we are</p>
-                        <p>Store Locator</p>
-                        <p>Raya Installments</p>
+                        <h4>Download App</h4>
+                        <p><FaGooglePlay className='FaApple fs-6'/>Google Play</p>
+                        <p> <FaApple className='FaApple'/>  App Store</p>
+                        
                     </div>
                     <div>
-                        <h4>About Raya</h4>
-                        <p>Who we are</p>
-                        <p>Store Locator</p>
-                        <p>Raya Installments</p>
+                        <h4>Stay in the know</h4>
+                        <p>Subscribe to our newsletter</p>
+                        
+                        <p><MdOutlineEmail className='MdOutlineEmail'/><input type="text" className='inputFooter' placeholder='Email-(ex@gmail.com)'/> <BsArrowRightSquareFill className='BsArrowRightSquareFill'/></p>
                     </div>
                 </div>
                 <div className='footerLower'>footer lower</div>
